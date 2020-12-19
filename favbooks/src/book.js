9449ea822e602796f7c599aafa01c2f54b92c9bc
grumpy-cat <grumpy-cat@mac>
2020-12-19 15:26:18 +0100

    Destructure data in book component

--- favbooks/src/book.js
+++ favbooks/src/book.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-const book = () => {
+const book = ({ image, title, author }) => {
   return (
-    <div>
-      <img
-        src="https://imgcdn.saxo.com/_9780135957059"
-        alt="The Prgamatic Programmer 20th Anniersary Edition"
-      />
-      <h1>The Prgamatic Programmer 20th Anniersary Edition</h1>
-      <h2>Some Author</h2>
-    </div>
+    <article>
+      <img src={image} alt={title} height="200px" />
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+    </article>
   );
 };
 
